3444696e2345fc21749f381dc87908d4f40d259b
DKJazzu <dkjazzu@gmail.com>
2026-04-25 16:44:08 +0200

    fix: handle invalid input gracefully

--- element.sh
+++ element.sh
@@ -21,12 +21,15 @@ else
 # query the database
   RESULT=$($PSQL "SELECT e.atomic_number, e.name, e.symbol, t.type, p.atomic_mass, p.melting_point_celsius, p.boiling_point_celsius FROM elements e JOIN properties p USING(atomic_number) JOIN types t USING(type_id) WHERE $WHERE_CLAUSE;")
 
-if [[ ! -z "$RESULT" ]]
+# if no result is found
+  if [[ -z "$RESULT" ]]
   then
+    echo "I could not find that element in the database."
+  else
 # parse result 
     IFS="|" read -r NUM NAME SYMBOL TYPE MASS MELT BOIL <<< "$RESULT"
 # output 
-    echo "The element with atomic number $NUM is $NAME ($SYMBOL). It's a $TYPE, with a mass of $MASS amu. $NAME has a melting point of $MELT celsius and a boiling point of $BOIL celsius."
+    echo "The element with atomic number $NUM is $NAME ($SYMBOL). It's a $TYPE, with a mass of $MASS amu. $NAME has a melting point of $MELT celsius and a boiling point of $BOIL celsius." 
   fi
   
 fi
